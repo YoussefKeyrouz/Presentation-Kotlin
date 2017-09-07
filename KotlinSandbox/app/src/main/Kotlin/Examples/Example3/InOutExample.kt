b0148a439188839fd5294d061b386c4dfaac2a96
Youssef Keyrouz <youssef.keyrouz@gotocme.com>
2017-09-07 14:23:16 +0300

    Small modification after the presentation

--- KotlinSandbox/app/src/main/Kotlin/Examples/Example3/InOutExample.kt
+++ KotlinSandbox/app/src/main/Kotlin/Examples/Example3/InOutExample.kt
@@ -4,8 +4,8 @@ package Examples.Example3
  * Created by Y on 9/6/2017.
  */
 
-fun testInOut() {
-    val list : List<Child> = listOf(Child(), Child(), Child())
+fun testOut() {
+    val list : List<Child> = listOf(Child(1), Child(2), Child(3))
 
     //This works
     val list2 : List<Parent> = list
@@ -14,6 +14,6 @@ fun testInOut() {
 
 }
 
-fun testInOut2(comp : Comparable<Parent>) {
+fun testIn(comp : Comparable<Parent>) {
     var comp2 : Comparable<Child> = comp
 }
